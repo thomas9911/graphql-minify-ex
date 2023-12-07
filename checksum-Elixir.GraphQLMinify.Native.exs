b4ad0d0ee60d4404210dacafc5749edd5287423a
Thomas Timmer <thomas.timmer@bettyblocks.com>
2023-12-07 12:05:44 +0100

    fix: add checksums

---
+++ checksum-Elixir.GraphQLMinify.Native.exs
@@ -0,0 +1,12 @@
+%{
+  "graphql_minify_native-v0.1.0-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:9f243d8af6d81da0798d03a82a10cd6a0b2992aa8ede194808f050f7dff3a030",
+  "graphql_minify_native-v0.1.0-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:f5ceda76e9f7daa43f7151a1c1e32cb5d12290eed066be18ed9b165fa9f27aa9",
+  "libgraphql_minify_native-v0.1.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:17dddf398ff06779d013ea539686f165c0ae20d909ac0949252c27d188ba7911",
+  "libgraphql_minify_native-v0.1.0-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:4abd4fafea2747fc7c50517cc3c966b2e2de4a67da6da835f63b196b2ae7459c",
+  "libgraphql_minify_native-v0.1.0-nif-2.16-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:2b35d69696e1e4b076616c1139327aaaf5a520aee4bbe4c20892519320dcf879",
+  "libgraphql_minify_native-v0.1.0-nif-2.16-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:e9b05e6b9fdd66d080caf01ed65442c6cad7141b0915a36afd090be75f40009b",
+  "libgraphql_minify_native-v0.1.0-nif-2.16-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:8b4e9ed6fda93ac9d6415df58af592a488f9d6c1b30b0611631ad89e4b1e5171",
+  "libgraphql_minify_native-v0.1.0-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:ec4f16085e431a835f0115a1069e05316bc1841222062a14a3196296123ca7a1",
+  "libgraphql_minify_native-v0.1.0-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:a0f7bf8779f10c6b5a6e2868a3eed4e0b5eb98962e6c541b0073ff508eb5ffd4",
+  "libgraphql_minify_native-v0.1.0-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:aa47d5ab8f3ba9b1555efbb338bcf7b7c89e6751e49180dbe334b1f5fe8e834d",
+}
